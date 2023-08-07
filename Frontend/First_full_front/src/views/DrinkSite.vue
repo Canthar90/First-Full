9e88559a1d43879d32b671fbdbf3ab2847b645be
Canthar90 <d.cieslak90@gmail.com>
2023-08-07 14:07:33 +0200

    Improving structure

--- Frontend/First_full_front/src/views/DrinkSite.vue
+++ Frontend/First_full_front/src/views/DrinkSite.vue
@@ -4,36 +4,12 @@
       <figure
         class="mt-8 bg-slate-800 rounded-xl md:p-8 col-start-4 col-span-4 justify-center gap-x-6"
       >
-        <div v-if="!drinkStore.randomDrinkCollapseFlag">
-          <div class="text-orange-200">
-            Are you tired of the same old drinks and looking for an exciting new fling for your
-            taste buds? Look no further, because we have the ultimate concoction of romance and
-            mixology waiting just for you!
-          </div>
-          <div class="text-orange-200 pt-4 pb-4">
-            Introducing "Mix & Mingle" - your go-to drink recipe matchmaker that will sweep you off
-            your feet and leave you thirsting for more! Our random drink recipe generator is like a
-            dating site for your taste buds, creating delightful and unexpected connections that
-            will leave you wanting to say, "Cheers!"
-          </div>
-        </div>
-
-        <div class="text-white col-start-2 flex justify-end px-12">
-          <action-button
-            text="Find Your Match"
-            buttonType="primary"
-            @click="randomDrink"
-          ></action-button>
-        </div>
+        <random-drink></random-drink>
       </figure>
     </div>
   </body>
 </template>
 
 <script lang="ts" setup>
-import ActionButton from '@/components/Shared/ActionButton.vue'
-import { useDrinkStore } from '@/stores/drinks'
-
-const drinkStore = useDrinkStore()
-const randomDrink = drinkStore.RANDOM_DRINK_INIT
+import RandomDrink from '@/components/RandomDrink/RandomDrink.vue'
 </script>
