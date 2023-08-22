a17a4005d2dddae70149e87a7517f708ea37860c
Canthar90 <d.cieslak90@gmail.com>
2023-08-22 16:48:51 +0200

    Added api data types

--- Frontend/First_full_front/src/api/types.ts
+++ Frontend/First_full_front/src/api/types.ts
@@ -18,3 +18,42 @@ export interface drinksByIngredient {
   end_flag: boolean
   end_message: message[]
 }
+
+export interface cvDataContactInfo {
+  Age: number
+  City: string
+  Email: string
+  Phone: string
+}
+
+export interface cvDataEducation {
+  Degree: string
+  'Thesis Topic': string
+  University: string
+  Year: string
+}
+
+export interface cvDataExperience {
+  Company: string
+  Duration: string
+  Position: string
+  Responsibilities: [string]
+}
+
+export interface cvDataSkill {
+  Percentage: string
+  Skil: string
+}
+
+export interface cvDataFrame {
+  'Contact Information': cvDataContactInfo
+  Education: [cvDataEducation]
+  'Full Name': string
+  Github: string
+  Hobbies: string
+  Linkedin: string
+  Resume: [cvDataExperience]
+  Skills: [cvDataSkill]
+  Summary: string
+  Title: string
+}
