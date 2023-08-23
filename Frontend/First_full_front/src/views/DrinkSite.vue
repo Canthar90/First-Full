0254497a8b40a2c72b0cd6ce67dbfd22ce170445
Canthar90 <d.cieslak90@gmail.com>
2023-08-23 14:03:17 +0200

    Improving responsibilityu of side design

--- Frontend/First_full_front/src/views/DrinkSite.vue
+++ Frontend/First_full_front/src/views/DrinkSite.vue
@@ -8,13 +8,13 @@
       </figure>
 
       <figure
-        class="mt-8 bg-slate-800 rounded-xl p-8 col-start-4 md:col-start-2 col-span-4 md:col-span-8 justify-center gap-x-6"
+        class="mt-8 bg-slate-800 rounded-xl p-8 col-start-1 sm:col-start-2 md:col-start-3 lg:col-start-4 col-span-10 sm:col-span-8 md:col-span-6 lg:col-span-4 justify-center gap-x-6"
       >
         <drink-by-name />
       </figure>
 
       <figure
-        class="mt-8 bg-slate-800 rounded-xl -8 col-start-4 md:col-start-2 col-span-4 md:col-span-8 justify-center gap-x-6"
+        class="mt-8 bg-slate-800 rounded-xl p-8 col-start-1 sm:col-start-2 md:col-start-3 lg:col-start-4 col-span-10 sm:col-span-8 md:col-span-6 lg:col-span-4 justify-center gap-x-6"
       >
         <drinks-by-ingredient />
       </figure>
