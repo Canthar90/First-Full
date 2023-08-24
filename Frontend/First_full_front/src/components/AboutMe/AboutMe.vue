2a1ab88c8e3840d07de04984ad39072b6ce6ea6b
Canthar90 <d.cieslak90@gmail.com>
2023-08-24 16:54:46 +0200

    Added some data to AboutMe.vue

--- Frontend/First_full_front/src/components/AboutMe/AboutMe.vue
+++ Frontend/First_full_front/src/components/AboutMe/AboutMe.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="text-orange-200 grid grid-cols-4">
-    <figure class="mt-8 p-8 bg-slate-800 rounded-xl col-start-1 col-span-4">
+  <div class="text-orange-200 grid grid-cols-6 gap-2">
+    <figure class="mt-8 p-8 bg-slate-800 rounded-xl col-start-1 col-span-6">
       <h2 class="flex text-xl justify-center pb-4">Hi I'm Dawid Cieślak</h2>
       <div
         class="flex justify-left pt-4"
@@ -8,6 +8,47 @@
         :key="element"
       >
         <p>{{ element }}</p>
+      </div>
+    </figure>
+    <div class="col-span-6 grid grid-cols-6">
+      <figure
+        v-for="education in aboutStore.cvEducation"
+        :key="education.Degree"
+        class="mt-8 p-8 bg-slate-800 rounded-xl col-span-2 col-start-3"
+      >
+        <div class="grid grid-cols-3 gap-2">
+          <h1 class="text-xl col-span-3">📖 Education:</h1>
+          <div class="text-md col-span-2">
+            <h2 class="flex">{{ education.University }}</h2>
+          </div>
+          <div>
+            <p class="flex justify-start text-sm">{{ education.Year }}</p>
+          </div>
+          <div class="col-span-2 row-span-2">
+            <p>{{ education['Thesis Topic'] }}</p>
+          </div>
+        </div>
+      </figure>
+    </div>
+
+    <div class="col-span-6 grid grid-cols-6">
+      <div class="col-start-3 col-span-2 mt-8 p-8 bg-slate-800 rounded-xl">
+        <h1 class="text-xl flex justify-center">⚙️ Experience:</h1>
+      </div>
+    </div>
+    <figure
+      v-for="experience in aboutStore.cvExperience"
+      :key="experience.Duration"
+      class="mt-8 p-8 bg-slate-800 rounded-xl col-span-2 border border-orange-300"
+    >
+      <div class="grid gap-2">
+        <h1 class="text-xl">{{ experience.Position }}:</h1>
+        <p class="flex justify-end text-sm pb-4">{{ experience.Duration }}</p>
+        <p class="">At: {{ experience.Company }}</p>
+        <p class="pb-2">Responsibilites:</p>
+        <ul class="list-disc list-inside">
+          <li v-for="elem in experience.Responsibilities" :key="elem" class="pb-2">{{ elem }}</li>
+        </ul>
       </div>
     </figure>
   </div>
