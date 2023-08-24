bddc86af72b4b86f61e09c2c02c85eaac37d35b3
Canthar90 <d.cieslak90@gmail.com>
2023-08-24 12:47:51 +0200

    Added test to about store testing fething data from api

--- Frontend/First_full_front/tests/unit/Stores/about.test.ts
+++ Frontend/First_full_front/tests/unit/Stores/about.test.ts
@@ -4,6 +4,9 @@ import axios from 'axios'
 
 import { useAboutStore } from '@/stores/about'
 
+vi.mock('axios')
+const axiosGetMock = axios.get as Mock
+
 describe('Initial state', () => {
   beforeEach(() => {
     setActivePinia(createPinia())
@@ -37,5 +40,76 @@ describe('Initial state', () => {
   it('stores initial value of cvDataFetched', () => {
     const store = useAboutStore()
     expect(store.cvDataFetched).toEqual(false)
+  })
+})
+
+describe('Functionality', () => {
+  describe('API related functions', () => {
+    const mockApi = () => {
+      const ContactInformation = {
+        Age: 27,
+        City: 'City',
+        Email: 'Email',
+        Phone: '1234'
+      }
+
+      const education = [
+        {
+          Degree: 'degree',
+          'Thesis Topic': 'topic',
+          University: 'university',
+          Year: 'year'
+        }
+      ]
+
+      const experience = [
+        {
+          Company: 'company',
+          Duration: 'duration',
+          Position: 'position',
+          Responsibilities: ['responsibility']
+        }
+      ]
+
+      const skills = [
+        {
+          Percentage: '42%',
+          Skil: 'skil'
+        }
+      ]
+
+      const finalData = {
+        'Contact Information': ContactInformation,
+        Education: education,
+        'Full Name': 'name',
+        Github: 'github',
+        Hobbies: 'hobby',
+        Linkedin: 'linkedin',
+        Resume: experience,
+        Skills: skills,
+        Summary: 'summary',
+        Title: 'title'
+      }
+
+      axiosGetMock.mockResolvedValue({
+        data: finalData
+      })
+
+      return { finalData, skills, experience, education, ContactInformation }
+    }
+    describe('GET_CV_DATA', () => {
+      it('Checks if all data is fetched from api and correctly updated', async () => {
+        const { finalData, skills, experience, education, ContactInformation } = mockApi()
+        const store = useAboutStore()
+        await store.GET_CV_DATA()
+
+        expect(store.cvData).toEqual(finalData)
+        expect(store.cvEducation).toEqual(education)
+        expect(store.cvContactInfo).toEqual(ContactInformation)
+        expect(store.cvExperience).toEqual(experience)
+        expect(store.cvSkills).toEqual(skills)
+        expect(store.cvDataFetched).toBe(true)
+      })
+    })
   })
 })
